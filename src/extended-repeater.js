64ded653b94fb594e59a53581cd7b596f05a138a
velteren <senior.saveliev2012@yandex.ru>
2021-03-06 18:14:13 +0300

    init: make pre-solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  str = str + '';
+  let separator, additionSeparator, repeatTimes, addition, additionRepeatTimes;
+  let result = [];
+  result[0] = str;
+  for (let option in options) {
+    if (!(repeatTimes in options)) {repeatTimes = 1} else {repeatTimes = options[repeatTimes]}
+    if (!(separator in options)) {separator = '+'} else {separator = options[separator]}
+    if (!(addition in options)) {addition = ''} else {addition = options[addition]}
+    if (!(additionRepeatTimes in options)) {additionRepeatTimes = 1} else {additionRepeatTimes = options[additionRepeatTimes]}
+    if (!(additionSeparator in options)) {additionSeparator = '|'} else {additionSeparator = options[additionSeparator]}
+  }
+  let counter = repeatTimes;
+  let tmparr = [];
+  tmparr[0] = addition;
+  for (let i = 0; i < additionRepeatTimes; i++) {
+    if (i !== additionRepeatTimes - 1) {
+      tmparr.push(additionSeparator);
+      tmparr.push(addition);
+    }
+  }
+  let addition_union = tmparr.join('');
+  while (counter) {
+    result.push(addition_union);
+    result.push(separator);
+    counter -= 1;
+  }
+  return result.join('');
+
 };
   
