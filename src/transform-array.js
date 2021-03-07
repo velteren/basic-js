de5228c452e39ed587cbfebf060895d109807f7b
velteren <senior.saveliev2012@yandex.ru>
2021-03-07 20:57:43 +0300

    fix: rework all solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -4,14 +4,24 @@ module.exports = function transform(arr) {
   let transformed = [];
 	if (!Array.isArray(arr)) throw new Error ('Error');
   if (!(arr.includes('--discard-next') || arr.includes('--discard-prev') || arr.includes('--double-next') || arr.includes('--double-prev'))) return arr;
-  for (let i=0; i < arr.length; i++) {
-    if (arr[i] == '--discard-next' && arr[i+2] == '--double-prev') i += 2;
-    if (arr[i] == '--discard-next' && arr[i+2] == '--discard-prev') i += 2;
-    if (arr[i] == '--discard-next' && i < arr.length - 1) i += 1;
-    if (arr[i] == '--discard-prev' && arr[i-2] !== '--discard-next') transformed.pop();
-    if (arr[i] == '--double-next' && i < arr.length - 1) transformed.push(arr[i+1]);
-    if (arr[i] == '--double-prev' && i > 0 && arr[i-2] !== '--discard-next') transformed.push(arr[i-1]);
-    if (arr[i] !== '--discard-next' && arr[i] !== '--discard-prev' && arr[i] !== '--double-next' && arr[i] !== '--double-prev') transformed.push(arr[i]);
+  for (let i = 0; i < arr.length; i++ ) {
+    switch(arr[i]) {
+      case '--discard-next':
+        i += 1;
+        break;
+      case '--discard-prev':
+        if (i !== 0 && arr[i-2] !== '--discard-next') transformed.pop();
+        break;
+      case '--double-next':
+        if (i < arr.length - 1) transformed.push(arr[i+1]);
+        break;
+      case '--double-prev':
+        if (i > 0 && arr[i-2] !== '--discard-next') transformed.push(arr[i-1]);
+        break;
+      default:
+        transformed.push(arr[i]);
+        break;
+    }
   }
   return transformed;
 };
