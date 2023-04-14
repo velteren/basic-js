845d0f262f466f806056665c2e52c8579a21ce41
Konstantin <77935386+velteren@users.noreply.github.com>
2023-04-14 22:48:38 +0300

    feat: add vigenere cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,44 +20,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  constructor(bool) {
-    this.bool = bool;
-    if (this.bool === undefined) this.bool = true;
-  }
-  encrypt( m, k, mode ){
-	m = m.toUpperCase();
-  k = k.toUpperCase();
-	var a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	var maxlength = Math.max(m.length, k.length);
-	var r = '';
-	for(let i = 0; i < maxlength; i++) { 
-		var mi = a.indexOf( m[ ( (i>=m.length) ?i%m.length :i ) ] );
-		var ki_s = k[ ( (i>=k.length) ?i%k.length :i ) ];
-		var ki = (typeof mode !== 'undefined' && mode.indexOf('gronsfeld') !== -1) ? parseInt( ki_s ): a.indexOf( ki_s );
-		ki = ( (typeof mode !== 'undefined' && mode.indexOf('decrypt') !== -1) ?(-ki) :ki );
-		let c = a[ ( ( ( a.length + ( mi + ki ) ) % a.length ) ) ];	
-		c = (mode === 'shifted_atbash') ? a[a.length-1-a.indexOf(c)] : c;	
-		r += c;																
-	}
-	if (this.bool) {return r} else return r.split('').reverse().join(''); 
-}
-   
-  decrypt( m, k, mode ){
+  chars = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
   
-	  var a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	  var maxlength = Math.max(m.length, k.length);
-	  var r = '';
-	  for(let i=0; i<maxlength; i++){ 
-		  var mi = a.indexOf( m[ ( (i>=m.length) ?i%m.length :i ) ] );
-		  var ki_s = k[ ( (i>=k.length) ?i%k.length :i ) ];
-		  var ki = (typeof mode !== 'undefined' && mode.indexOf('gronsfeld') !== -1) ? parseInt( ki_s ): a.indexOf( ki_s );
-		  ki = ( (typeof mode !== 'undefined' && mode.indexOf('decrypt') !== -1) ?(-ki) :ki );
-		  let c = a[ ( ( ( a.length + ( mi + ki ) ) % a.length ) ) ];	
-		  c = (mode === 'shifted_atbash') ? a[a.length-1-a.indexOf(c)] : c;
-		  r += c;	
-	}
-	if (this.bool) {return r} else return r.split('').reverse().join('');
-}
+  constructor(bool) {
+    this.encryption = bool;
+  }
+
+  encrypt(msg, key) {
+    if (msg === undefined || key === undefined) throw new Error('Incorrect arguments!');
+    let i = 0;
+    const msgChars = msg.toUpperCase().split(''),
+          keyChars = key.toUpperCase().split(''),
+          encryptedArr = msgChars.map((item) => {
+            if (this.chars.includes(item)) {
+              if (i === key.length) i = 0;
+              let num = item.charCodeAt(0) + keyChars[i].charCodeAt(0) - 130;
+              if (num >= this.chars.length) {
+                item = String.fromCharCode(num - this.chars.length + 65);
+              } else if (num < 0) {
+                item = String.fromCharCode(num + this.chars.length + 65);
+              } else {
+                item = String.fromCharCode(num + 65);
+              }
+              i++;
+            }
+            return item;
+          });
+
+    return this.encryption === true || this.encryption === undefined
+      ? encryptedArr.join('')
+      : encryptedArr.reverse().join('');
+  }
+
+  decrypt(msg, key) {
+    if (msg === undefined || key === undefined) throw new Error('Incorrect arguments!');
+    let i = 0;
+    const msgChars = msg.toUpperCase().split(''),
+          keyChars = key.toUpperCase().split('');
+    const decryptedArray = msgChars.map((item) => {
+      if (this.chars.includes(item)) {
+        if (i === key.length) i = 0;
+        let num = item.charCodeAt(0) - keyChars[i].charCodeAt(0);
+        if (num < 0) {
+          item = String.fromCharCode(num + this.chars.length + 65);
+        } else {
+          item = String.fromCharCode(num + 65);
+        }
+        i++;
+      }
+      return item;
+    });
+
+    return this.encryption === true || this.encryption === undefined
+      ? decryptedArray.join('')
+      : decryptedArray.reverse().join('');
+  }
 }
 
 module.exports = {
