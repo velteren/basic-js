b0bded525282bcd97d2f59ebf387f7ef36734833
Constantine Saveliev <senior.saveliev2012@yandex.ru>
2025-01-06 15:12:41 +0300

    refactor: optimize createDreamTeam code

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,21 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
- function createDreamTeam(members) {
+function createDreamTeam(members) {
   if (Array.isArray(members) == false) return false;
   let names = [];
-	for (let i=0; i < members.length; i++) {
-  	if (typeof(members[i]) == 'string') {
-    	names.push(members[i].split(' ').join(''));
+  for (let i=0; i < members.length; i++) {
+    if (typeof(members[i]) == 'string') {
+      names.push(members[i].trim().toUpperCase());
     }
   }
   let acronym =[];
   for (let i=0; i < names.length; i++) {
-
-  	acronym.push(names[i].charAt(0));
+    acronym.push(names[i].charAt(0));
   }
-  
-  return acronym.join('').toUpperCase().split('').sort().join('');
+  return acronym.sort().join('');
 }
 
 module.exports = {
